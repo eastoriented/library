56387b55620107b911e4f822e8f3fec98c6a3ead
Frédéric Hardy <frederic.hardy@mageekbox.net>
2019-01-29 10:40:59 +0100

    Add `install.sh` script to use docker to install environment and run installation.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set -e
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: `docker` is not installed.' >&2
+  exit 1
+fi
+
+docker run -it --rm -v $(PWD):/src -w /src -v  $HOME/.composer:/.composer -e COMPOSER_CACHE_DIR=/.composer -e COMPOSER_ALLOW_SUPERUSER=1 -u $(id -u) composer:latest composer require --dev eastoriented/library dev-master
+
+./vendor/bin/eastoriented-library install
